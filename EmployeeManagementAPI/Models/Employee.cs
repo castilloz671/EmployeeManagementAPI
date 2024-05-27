3b87eb06e726d62c9ae5f657a52ba39b80069b0c
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-05-27 14:58:43 -0400

    Add project files.

---
+++ EmployeeManagementAPI/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [StringLength(10)]
+        public string Phone { get; set; }
+
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string Position { get; set; }
+
+        [StringLength(50)]
+        public string Office { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+
+        public decimal CalculateNetSalary()
+        {
+            decimal ARS = Salary * 0.0304M;
+            decimal AFP = Salary * 0.0287M;
+            return Salary - ARS - AFP;
+        }
+    }
+}
---
+++ EmployeeManagementAPI/Program.cs
@@ -0,0 +1,46 @@
+using EmployeeManagementAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<EmployeeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Use CORS
+app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
